f17ed055268810ca6a7981fe0b7873d04dd9cbcf
elliotec <elliotecweb@gmail.com>
2014-01-30 15:34:47 -0800

    add test framework

---
+++ test/features/create_player_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+feature "add new player" do
+  scenario "select player" do
+    visit root_path
+  end
+end
--- test/helpers/trophies_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TrophiesHelperTest < ActionView::TestCase
-end
